2296ea87998721e839d459e17da76c2f7b74fab8
Ruuskanen Vivi <ruuskvivi@gmail.com>
2024-06-25 11:25:14 +0300

    No modifications to part 2a example application

--- parts/part2/src/App.jsx
+++ parts/part2/src/App.jsx
@@ -1,11 +1,16 @@
 import Note from './components/Note'
 
+const Note = ({ note }) => {
+  return (
+    <li>{note.content}</li>
+  )
+}
+
 const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-
         {notes.map(note => 
           <Note key={note.id} note={note} />
         )}
