884845805e38d8151fc90c8c24dcff4624d264d0
Ruuskanen Vivi <ruuskvivi@gmail.com>
2024-07-02 15:02:33 +0300

    courseinformation 2.2 making variables for props + 2.3 done

--- parts/part2/courseinformation/src/components/Course.jsx
+++ parts/part2/courseinformation/src/components/Course.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
 
 const Course = (props) => {
+  const {course} = props
   console.log('Course props value is', props)
   return (
     <div>
-      <Header courseName = {props.course.name} />
-      <Content parts = {props.course.parts} />
-      <Total total = {props.course.parts} />
+      <Header courseName = {course.name} />
+      <Content parts = {course.parts} />
+      <Total parts = {course.parts} />
     </div>
   )
 }
 
 const Header = (props) => {
   console.log('Header props value is', props)
+  const {courseName} = props
       return (
       <h1>
-        {props.courseName}
+        {courseName}
       </h1>
   )
 }
 
 const Content = (props) => {
   console.log('Content props value is', props)
+  const {parts} = props
   return (
       <div>
-        {props.parts.map(part => (
+        {parts.map(part => (
           < Part key = {part.id} name = {part.name} exercise = {part.exercises} />
         ))}
       </div>
@@ -43,10 +46,11 @@ const Part = (props) => {
 
 const Total = (props) => {
   console.log('Total props value is', props)
-  const total = props.total.reduce((total, part) => total + part.exercises, 0)
+  const {parts} = props
+  const total = parts.reduce((s, p) => s + p.exercises, 0)
   return (
       <p>
-        total of {total} exercises
+        <strong>total of {total} exercises</strong>
       </p>
   )
 }
