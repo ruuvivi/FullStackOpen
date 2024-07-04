61822c8e3be626089d81057751c917fa24482e01
Ruuskanen Vivi <ruuskvivi@gmail.com>
2024-07-04 14:47:52 +0300

    courseinformation 2.4 and 2.5 done

--- parts/part2/courseinformation/src/components/Course.jsx
+++ parts/part2/courseinformation/src/components/Course.jsx
@@ -1,52 +1,62 @@
 import React from 'react';
 
-const Course = (props) => {
-  const {course} = props
-  console.log('Course props value is', props)
+const Course = ({courses}) => {
+  console.log('Course props value is', courses)
+  const header = 'Web development curriculum'
   return (
     <div>
-      <Header courseName = {course.name} />
-      <Content parts = {course.parts} />
-      <Total parts = {course.parts} />
+      <Header header ={header} />
+      {courses.map(course => (
+        <div key={course.id}>
+          <SubHeader name={course.name} />
+          <Content parts={course.parts} />
+          <Total parts={course.parts} />
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-const Header = (props) => {
-  console.log('Header props value is', props)
-  const {courseName} = props
+const Header = ({header}) => {
+  console.log('Header props value is', header)
       return (
       <h1>
-        {courseName}
+        {header}
       </h1>
   )
 }
 
-const Content = (props) => {
-  console.log('Content props value is', props)
-  const {parts} = props
+const SubHeader = ({name}) => {
+  console.log('Header props value is', name)
+      return (
+      <h2>
+        {name}
+      </h2>
+  )
+}
+
+const Content = ({parts}) => {
+  console.log('Content props value is', parts)
   return (
       <div>
         {parts.map(part => (
-          < Part key = {part.id} name = {part.name} exercise = {part.exercises} />
+          < Part key={part.id} name={part.name} exercises={part.exercises} />
         ))}
       </div>
     )
 }
 
-const Part = (props) => {
-  console.log('Part props value is', props)
-  const {name, exercise} = props
+const Part = ({name, exercises}) => {
+  console.log('Part props value is', name, exercises)
   return (
       <p>
-        {name} {exercise}
+        {name} {exercises}
       </p>
     )
 }
 
-const Total = (props) => {
-  console.log('Total props value is', props)
-  const {parts} = props
+const Total = ({parts}) => {
+  console.log('Total props value is', parts)
   const total = parts.reduce((s, p) => s + p.exercises, 0)
   return (
       <p>
