b2f948837da98b9c80778cc60c8696fe470fa9c8
Ruuskanen Vivi <ruuskvivi@gmail.com>
2024-07-15 11:06:28 +0300

    phonebook 2.9 wip

--- parts/part2/phonebook/src/App.jsx
+++ parts/part2/phonebook/src/App.jsx
@@ -10,46 +10,66 @@ const Person = ({ name, number }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456'}
+    { name: '', number: '', id: ''}
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [showFound, setShowFound] = useState(true)
 
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length + 1,
+      findName: showFound
     }
 
     if (persons.find(existing => existing.name === newName)) {
       alert(newName + ' is already added to phonebook')
       setNewName('')
       setNewNumber('')
+      setShowFound('')
     }
     else {
       setPersons(persons.concat(personObject))
       setNewName('')
       setNewNumber('')
+      setShowFound('')
       console.log('button clicked', event.target)
   
     }
   }
 
+  const foundNamestoShow = showFound
+  ? persons
+  : persons.filter(found => found.name.toLowerCase().includes(showFound.toLowerCase()))
+
   const handleNameChange = (event) => {
-    console.log('event target value', event.target.value)
+    console.log('newName', event.target.value)
     setNewName(event.target.value)
   }
 
   const handleNumberChange = (event) => {
-    console.log('event target value', event.target.value)
+    console.log('newNumber', event.target.value)
     setNewNumber(event.target.value)
+  }
+
+  const handleFoundChange = (event) => {
+    console.log('showfound', event.target.value)
+    setShowFound(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson} >
+      <div>filter shown with
+          <input
+            value={showFound}
+            onChange={handleFoundChange}
+          />
+          </div>
         <div>name:
           <input
             value={newName}
@@ -66,12 +86,10 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-        <div>debug: {newName}</div>
+        <div>debug: {newName} {newNumber}</div>
         <h2>Numbers</h2>
         <div>
-          {persons.map((person, index) =>
-            <Person key={index} name={person.name} number={person.number} />
-          )}
+        {showFound ? foundNamestoShow : persons.map((person, index) => <Person key={index} name={person.name} number={person.number}/>) }
         </div>
     </div>
   )
