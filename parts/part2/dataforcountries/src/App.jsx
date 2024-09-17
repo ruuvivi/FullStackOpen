926b3e9b50d54ed415efb5b581c1fc7ffcb8a9d3
Ruuskanen Vivi <ruuskvivi@gmail.com>
2024-09-17 08:56:57 +0300

    2.19 done

--- parts/part2/dataforcountries/src/App.jsx
+++ parts/part2/dataforcountries/src/App.jsx
@@ -1,71 +1,93 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const Button = (props) => { 
+  console.log('Button props value is', props)
+  const { handleClick, text } = props;
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
 const App = () => {
   const [value, setValue] = useState('')
   const [information, setInformation] = useState([])
   const [filteredCountries, setFilteredCountries] = useState([])
+  const [selectedCountry, setSelectedCountry] = useState(null)
+
+  const handleClick = (country) => {
+    setSelectedCountry(country);
+  };
 
   useEffect(() => {
-    console.log('effect run, country is now', filteredCountries)
-
-    // skip if country is not defined
     if (value) {
-      console.log('fetching information...')
-      axios
+      console.log('effect run, country is now', filteredCountries)
+      if (value) {
+        console.log('fetching information...')
+        axios
         .get(`https://restcountries.com/v3.1/name/${value}`)
         .then(response => {
           setInformation(response.data)
         })
-    }
-    else {
-      setInformation([])
-    }
+    } else {
+      setInformation([]) // clear information if no search input
+    }}
   }, [value])
 
   const handleChange = (event) => {
     setValue(event.target.value)
+    setSelectedCountry(null); // Clear the information when input changes
   }
 
   const onSearch = (event) => {
-      event.preventDefault()
-      setCountriesFiltered(value)
-    }
+    event.preventDefault();
+    setCountriesFiltered(value)
+    setSelectedCountry(null);
+  }
 
   const queryList = () => {
     if (filteredCountries.length > 10) {
-      return (
-      <p>Too many matches, specify another filter</p>
-      )
+      return <p>Too many matches, specify another filter</p>;
     }
     else if (filteredCountries.length > 1) {
       return (
         <div>
           {filteredCountries.map(country => (
-            <ul>{country.name.common}</ul>
+            <ul key={country.name.common}>
+              {country.name.common}
+              <Button handleClick={() => handleClick(country)} text="show" />
+            </ul>
           ))}
-          </div>
-      )
-  } else if (filteredCountries.length === 1) {
-    const country = filteredCountries[0]
-    return (
-      <div>
-          <h1>{country.name.common}</h1>
-          <p>capital {country.capital}</p>
-          <p>area {country.area} km²</p>
-          <h3>languages:</h3>
-          <li>
-            {Object.values(country.languages).map((language, index) => (
-              <li key={index}>{language}</li>
-            ))}
-          </li>
-          <img
-            src={country.flags.svg}
-            style={{ width: '200px', height: '200px' }}
-          />
         </div>
       )
+    } else if (filteredCountries.length === 1) {
+      // Directly show the single country's details
+      const country = filteredCountries[0];
+      return showselectedCountry(country);
     }
+  }
+
+  const showselectedCountry = (country) => {
+    return (
+      <div>
+        <h1>{country.name.common}</h1>
+        <p>capital {country.capital}</p>
+        <p>area {country.area} km²</p>
+        <h3>languages:</h3>
+        <ul>
+          {Object.values(country.languages).map((language, index) => (
+            <li key={index}>{language}</li>
+          ))}
+        </ul>
+        <img
+          src={country.flags.svg}
+          style={{ width: '200px', height: '200px' }}
+          alt={`Flag of ${country.name.common}`}
+        />
+      </div>
+    );
   }
 
   useEffect(() => {
@@ -74,8 +96,8 @@ const App = () => {
       information.filter(country =>
         country.name.common.toLowerCase().includes(value.toLowerCase())
       )
-    )
-  }, [value, information])
+    );
+  }, [value, information]);
 
   return (
     <div>
@@ -83,6 +105,8 @@ const App = () => {
         find countries <input value={value} onChange={handleChange} />
       </form>
       {queryList()}
+      {/* Show selected country details if a country is selected */}
+      {selectedCountry && showselectedCountry(selectedCountry)}
     </div>
   )
 }
